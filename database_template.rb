fef4865d8067cb936bfbd89fc40ce696c0c5c89d
Shiva Bhusal <shivabhusal@users.noreply.github.com>
2017-07-27 22:55:31 +0545

    Adds sqlserver template

--- database_template.rb
+++ database_template.rb
@@ -71,16 +71,54 @@ production:
   database: db/production.sqlite3
 SQLITE
 
+# --------------------- MS-SQL Server ------------------------------
+SQL_SERVER_CONFIG = <<-SQLSERVER
+default: &default
+  adapter: sqlserver
+  encoding: utf8
+  host: localhost
+  dataserver:
+  port: 1433
+  username: arxfit
+  password: P@ssword
+  pool: 5
+  timeout: 5000
+  azure: true
+  login_timeout: 5000
+
+development:
+  <<: *default
+  database: <%= app_name %>_development
+
+production:
+  <<: *default
+  adapter: sqlserver
+  encoding: utf8
+  host: something.database.secure.windows.net
+  dataserver:
+  port: 1433
+  username: myapp
+  password: P@ssword
+  pool: 5
+  timeout: 5000
+  azure: true
+  login_timeout: 5000
+SQLSERVER
+
 if ENV['db'] =~ /mysql/
   file 'config/database.yml', ERB.new(MYSQL_CONFIG).result(binding)
 elsif ENV['db'] =~ /pg/ || ENV['db'] =~/post/
   file 'config/database.yml', ERB.new(PG_CONFIG).result(binding)
 elsif ENV['db'] =~/sqlite/
   file 'config/database.yml', ERB.new(SQLITE_CONFIG).result(binding)
+elsif ENV['db'] =~/server/
+  file 'config/database.yml', ERB.new(SQL_SERVER_CONFIG).result(binding)
 else
   puts 'Please pass a command like '
   puts "rails app:template path/to/template.rb db=mysql"
   puts "rails app:template path/to/template.rb db=pg"
   puts "rails app:template path/to/template.rb db=postgre"
   puts "rails app:template path/to/template.rb db=sqlite"
+  puts "rails app:template path/to/template.rb db=sqlserver"
 end
+
